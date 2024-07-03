9122589b68eeda41c4f43d408d2950b16b172ef5
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-07-03 10:52:30 +0600

    basic syntex practise of oop

---
+++ interview/oop.js
@@ -0,0 +1,11 @@
+class Human {
+    constructor(name, age, address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+}
+
+const user1 = new Human('Omar', 12, 'Dhaka');
+
+console.log(user1.address); // Output: 12
