c07b0a47dd2386c30d8f94cd3243bebc3e37a1d3
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-01 19:49:45 +0600

    feat: this keyword excerices

---
+++ interview/thisKeyWord.js
@@ -0,0 +1,18 @@
+// function unNamed(){
+//     // 'use strict'
+//     this.name = 'omar'
+// }
+
+// console.log(unNamed())
+// console.log(name)
+
+
+const testObject = {
+    name: 'omar',
+    age: 35,
+    profession: 'Developer',
+    bio:{
+        experience: 5,
+    }
+
+}
