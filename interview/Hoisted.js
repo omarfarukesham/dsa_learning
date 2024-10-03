51e07c6613bec5010cd95635d183febebe7f827d
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-03 08:13:45 +0600

    feat: hoisting practise again

--- interview/Hoisted.js
+++ interview/Hoisted.js
@@ -1,10 +1,33 @@
-var  x;
-console.log(x)
-x = 5
-console.log(x)
+// var  x;
+// console.log(x)
+// x = 5
+// console.log(x)
 
-hoistedFun()
+// hoistedFun()
 
- const helloHoited = function hoistedFun(){
-    console.log('Hello hoisted')
+//  const helloHoited = function hoistedFun(){
+//     console.log('Hello hoisted')
+// }
+
+
+function countCoffeeRegistration(counterName){
+    let count = 0;
+    return function(){
+            count++
+            console.log(`${counterName} Coffe served no ${count}`)
+        }
+    
 }
+
+const counter1 = countCoffeeRegistration('counter1')
+counter1()
+counter1()
+counter1()
+counter1()
+
+console.log('.............................')
+
+const counter2 = countCoffeeRegistration('coutner2')
+counter2()
+counter2()
+counter2()
