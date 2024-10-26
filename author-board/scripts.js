0380a381ea25d269fceb3d8e6ab85d0e188cc972
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-26 10:15:47 +0600

    feat: 43 steps has been done for this project

--- author-board/scripts.js
+++ author-board/scripts.js
@@ -32,7 +32,10 @@ const forumCategory = (id) => {
   const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
   const linkText = selectedCategory.category;
   const linkClass = `category ${selectedCategory.className}`;
-  return `<a href="${url}" ></a>`;
+
+  return `<a href="${url}" class="${linkClass}" target="_blank">
+    ${linkText}
+  </a>`;
 };
 
 const timeAgo = (time) => {
@@ -67,6 +70,8 @@ const viewCount = (views) => {
   return views;
 };
 
+
+
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
@@ -99,6 +104,7 @@ const showLatestPosts = (data) => {
     <tr>
       <td>
         <p class="post-title">${title}</p>
+        ${forumCategory(category_id)}
       </td>
       <td></td>
       <td>${posts_count - 1}</td>
@@ -107,4 +113,3 @@ const showLatestPosts = (data) => {
     </tr>`;
   }).join("");
 };
-
