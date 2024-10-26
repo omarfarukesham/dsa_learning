fe716c24a28a0ce3c8f5112c8ac7ecf12702934b
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-26 10:53:25 +0600

    feat: 52 to 54  steps has been done for this project

--- author-board/scripts.js
+++ author-board/scripts.js
@@ -71,17 +71,18 @@ const viewCount = (views) => {
 };
 
 const avatars = (posters, users) => {
-  return posters.map((poster) => {
-    const user = users.find((user) => user.id === poster.user_id);
-    if (user) {
-      const avatar = user.avatar_template.replace(/{size}/, 30);
-      const userAvatarUrl = avatar.startsWith("/user_avatar/")
-        ? avatarUrl.concat(avatar)
-        : avatar;
-      return `<img src="${userAvatarUrl}" alt="${user.name}" />`;
-    }
-  })
-
+  return posters
+    .map((poster) => {
+      const user = users.find((user) => user.id === poster.user_id);
+      if (user) {
+        const avatar = user.avatar_template.replace(/{size}/, 30);
+        const userAvatarUrl = avatar.startsWith("/user_avatar/")
+          ? avatarUrl.concat(avatar)
+          : avatar;
+        return `<img src="${userAvatarUrl}" alt="${user.name}" />`;
+      }
+    })
+    .join("");
 };
 
 const fetchData = async () => {
@@ -116,9 +117,14 @@ const showLatestPosts = (data) => {
     <tr>
       <td>
         <p class="post-title">${title}</p>
+
         ${forumCategory(category_id)}
       </td>
-      <td></td>
+      <td>
+        <div class="avatar-container">
+          ${avatars(posters, users)}
+        </div>
+      </td>
       <td>${posts_count - 1}</td>
       <td>${viewCount(views)}</td>
       <td>${timeAgo(bumped_at)}</td>
