0f64e6e7648b6047e84d556789576d1d08cf4670
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-27 11:10:13 +0600

    feat: fixThis key word practise from skilled dev

--- skilled-dev/fixThis.js
+++ skilled-dev/fixThis.js
@@ -22,4 +22,25 @@ console.log(typeof getUser)
 
 console.log(getUser.getName())
 console.log(getUser.getName())
-console.log(getUser.getName())
+console.log(getUser.printSeatNo())
+
+const StudentInfo = {
+    name: 'Isham',
+    age: 8,
+    school: {
+        name: 'SCD',
+        level: 3,
+        schoolInf(){
+            console.log(`His school name is ${this.name} and he is the level ${this.level}
+                s student`)
+        }
+    },
+    admission(){
+        console.log(`Mr. ${this.name} is going to addmit this year when he will be the ${this.age}`)
+    }
+
+}
+
+
+StudentInfo.school.schoolInf()
+StudentInfo.admission()
