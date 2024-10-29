8b2435897b5826ee235304675dd20d78917d7b74
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-29 10:41:25 +0600

    feat: array filter with vanila js has been practise

--- skilled-dev/arrayMethods.js
+++ skilled-dev/arrayMethods.js
@@ -1,6 +1,6 @@
 //filters practise
 
-const arr = [4, 5,6,7,8,9]
+// const arr = [4, 5,6,7,8,9]
 
 // const getFilterArray = arr.filter(ar => ar % 2 === 0)
 // console.log(getFilterArray)
@@ -11,6 +11,26 @@ const arr = [4, 5,6,7,8,9]
 // console.log(getReduceArrResult)
 
 //forEach method
- var sum = 0
-arr.forEach(x => sum += x)
-console.log('forEach ........ ', sum)
+//  var sum = 0
+// arr.forEach(x => sum += x)
+// console.log('forEach ........ ', sum)
+
+const array = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < array.length; i++) {
+//   array[i] = array[i] * 2;
+// }
+const newArr  = array.map(ar => ar * 2)
+
+console.log(newArr); // [2, 4, 6, 8, 10];
+
+const newFilterArr = array.filter(arr => arr % 2 === 0)
+console.log(newFilterArr)
+
+ const newEvenArr = []
+for(let i = 0; i < array.length; i++){
+    if(array[i] % 2 === 0){
+        newEvenArr.push(array[i])
+    }
+}
+console.log(newEvenArr)
