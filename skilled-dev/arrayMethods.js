a0a9cb4c2a4fad6e1bd44e37d46acf0556fc40b0
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-29 10:28:30 +0600

    feat: forEach has been practise

--- skilled-dev/arrayMethods.js
+++ skilled-dev/arrayMethods.js
@@ -2,10 +2,15 @@
 
 const arr = [4, 5,6,7,8,9]
 
-const getFilterArray = arr.filter(ar => ar % 2 === 0)
-console.log(getFilterArray)
+// const getFilterArray = arr.filter(ar => ar % 2 === 0)
+// console.log(getFilterArray)
 
 
-const getReduceArrResult = arr.reduce((acc, x)=> acc + x, 0)
+// const getReduceArrResult = arr.reduce((acc, x)=> acc + x, 0)
 
-console.log(getReduceArrResult)
+// console.log(getReduceArrResult)
+
+//forEach method
+ var sum = 0
+arr.forEach(x => sum += x)
+console.log('forEach ........ ', sum)
