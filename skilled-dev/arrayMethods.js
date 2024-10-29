64b2766cdcd6c0633b0d18bc5f1edbba5cb75662
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-29 10:04:23 +0600

    feat: array map has been practise

--- skilled-dev/arrayMethods.js
+++ skilled-dev/arrayMethods.js
@@ -1,2 +1,9 @@
 // array methods all .. 
 const users = ['Rumi', 'Raton', 'Rana', 'Raju']
+const newUsers = users.map(user => user+'s')
+console.log(newUsers)
+
+const squareArr = [2, 3, 4, 5]
+const newSquare = squareArr.map(sq => sq * sq)
+
+console.log(newSquare)
