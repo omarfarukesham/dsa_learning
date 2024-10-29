7df88edcbfb56d36613842ed5d07e79d649f108e
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-29 10:18:40 +0600

    feat: reduce method has been practise

--- skilled-dev/arrayMethods.js
+++ skilled-dev/arrayMethods.js
@@ -4,3 +4,8 @@ const arr = [4, 5,6,7,8,9]
 
 const getFilterArray = arr.filter(ar => ar % 2 === 0)
 console.log(getFilterArray)
+
+
+const getReduceArrResult = arr.reduce((acc, x)=> acc + x, 0)
+
+console.log(getReduceArrResult)
