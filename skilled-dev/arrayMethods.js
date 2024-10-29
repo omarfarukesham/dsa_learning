39cc2db3ef3b770994d963779289a390d5881a84
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-10-29 10:56:15 +0600

    feat: array filter with vanila js has been practise

--- skilled-dev/arrayMethods.js
+++ skilled-dev/arrayMethods.js
@@ -15,22 +15,41 @@
 // arr.forEach(x => sum += x)
 // console.log('forEach ........ ', sum)
 
-const array = [1, 2, 3, 4, 5];
+// const array = [1, 2, 3, 4, 5];
 
 // for (let i = 0; i < array.length; i++) {
 //   array[i] = array[i] * 2;
 // }
-const newArr  = array.map(ar => ar * 2)
+// const newArr  = array.map(ar => ar * 2)
 
-console.log(newArr); // [2, 4, 6, 8, 10];
+// console.log(newArr); // [2, 4, 6, 8, 10];
 
-const newFilterArr = array.filter(arr => arr % 2 === 0)
-console.log(newFilterArr)
+// const newFilterArr = array.filter(arr => arr % 2 === 0)
+// console.log(newFilterArr)
 
- const newEvenArr = []
-for(let i = 0; i < array.length; i++){
-    if(array[i] % 2 === 0){
-        newEvenArr.push(array[i])
+//  const newEvenArr = []
+// for(let i = 0; i < array.length; i++){
+//     if(array[i] % 2 === 0){
+//         newEvenArr.push(array[i])
+//     }
+// }
+// console.log(newEvenArr)
+
+const array = ['Jeni', 'Bob', 'Carol'];
+
+const isFriend = (friends, name) => {
+  for (let i = 0; i < friends.length; i++) {
+    const friend = friends[i];
+    if (friend === name) {
+      return true;
     }
+  }
+  return false;
 }
-console.log(newEvenArr)
+
+console.log(isFriend(array, 'Carol')); // true
+
+const findResult =  array.find(x => x === 'Carol')
+
+console.log(findResult)
+
